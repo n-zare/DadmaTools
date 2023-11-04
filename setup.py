63a7ec3d75cfd209a62990f17442012ad1c077b4
n-zare <n-zare@users.noreply.github.com>
2023-11-04 15:48:52 +0330

    setup.py

--- setup.py
+++ setup.py
@@ -39,7 +39,6 @@ setuptools.setup(
         "scikit-learn>=0.24.2",
         "Keras==2.4.3",
         "Keras-Preprocessing==1.1.0",
-        "tensorflow==2.2.0",
         "tokenizers==0.9.4",
         "hazm==0.7.0"
     ],
